2e6c2b3ef0dbab2ce651dfd98694dfd3afc58c79
franias <franciellipdias@gmail.com>
2019-08-07 16:14:10 -0300

    Refatora o teste parcializando o takeUntil para que só receba apenas a funcao como parametro

--- public/app/app.js
+++ public/app/app.js
@@ -3,14 +3,12 @@ import './utils/array_helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize } from './utils/operators.js'
 
-const doTake = takeUntil(2, ()=> {
-  console.log("oioio");
-  
-});
+const partializedTakeUntil = partialize(takeUntil, 2);
+const doTake = partializedTakeUntil(() => console.log("oi"));
 doTake();
 doTake();
-doTake();  
-
+doTake();
+doTake();
 
 const action = debounceTime(500,
   takeUntil(3, () =>

2fd444417e17db3d407de18a9e8877549f68d9b6
franias <franciellipdias@gmail.com>
2019-08-07 10:00:42 -0300

    Cria constante da funcao reduce, o somatorio dos itens agora esta guardado em sumItens

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,15 @@
 import { handleStatus, log } from './utils/promise_helpers.js';
 import './utils/array_helpers.js';
+
+const sumItens = notas => notas
+    .$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == '2143')
+    .reduce((total, item) => total + item.valor, 0);
 document
     .querySelector("#myButton")
     .onclick = () =>
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(notas => notas
-                .$flatMap(nota => nota.itens)
-                .filter(item => item.codigo == '2143')
-                .reduce((total, item) => total + item.valor, 0)
-            )
+            .then(sumItens)
             .then(console.log)
             .catch(console.log);

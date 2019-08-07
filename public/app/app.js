28752638a017050ad64421c7af4bf6f361144e3d
franias <franciellipdias@gmail.com>
2019-08-07 10:48:59 -0300

    Corrige bug no sumItens, faltava () na funcao listAll

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,10 @@
 import { log } from './utils/promise_helpers.js';
 import './utils/array_helpers.js';
 import { notasService as service } from './nota/service.js';
-const sumItens = code => notas => notas
-  .$flatMap(nota => nota.itens)
-  .filter(item => item.codigo == code)
-  .reduce((total, item) => total + item.valor, 0);
 document
   .querySelector("#myButton")
   .onclick = () =>
     service
-      .listAll()
-      .then(sumItens('2143'))
+      .sumItens('2143')
       .then(console.log)
       .catch(console.log);
--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -2,10 +2,24 @@ import { handleStatus } from '../utils/promise_helpers.js';
 
 const API = 'http://localhost:3000/notas';
 
+const sumItens = code => notas => 
+    notas.$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
+
 export const notasService = {
 
-    listAll(){
+    listAll() {
 
-        return fetch(API).then(handleStatus);
+        return fetch(API)
+        .then(handleStatus)
+        .catch(err => {
+            console.log(err);
+            return Promise.reject('Não foi possível obter as notas fiscais');
+        });
+    },
+    sumItens(code) {
+        
+        return this.listAll().then(sumItens(code));
     }
 };

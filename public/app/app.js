d96fc20c363abde9d3e6d45043f5984c549d180f
franias <franciellipdias@gmail.com>
2019-08-07 17:42:06 -0300

    Faz um timeOut da Promise usando o promise.race para limitar o tempo de espera da funcao, concorrendo com a segunda promise que ditará o limite de tempo

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,15 @@
-import { log } from './utils/promise_helpers.js';
+import { log, timeOutPromise } from './utils/promise_helpers.js';
 import './utils/array_helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js'
 
 const operations = pipe(
   partialize(takeUntil, 3),
-  partialize(debounceTime, 500)  
+  partialize(debounceTime, 500)
 );
 
 const action = operations(() =>
-  service
-    .sumItems('2143')
+  timeOutPromise(200, service.sumItems('2143'))
     .then(console.log)
     .catch(console.log)
 );
--- public/app/utils/promise_helpers.js
+++ public/app/utils/promise_helpers.js
@@ -3,4 +3,14 @@ export const handleStatus = res =>
 export const log = param => {
     console.log(param);
     return param;
-}
+};
+
+export const timeOutPromise = (milliseconds, promise) =>{
+    const timeout = new Promise((promise, reject)=>
+    setTimeout(() => reject('Limite da promise excedido.'),
+    milliseconds));
+
+    return Promise.race([
+        timeout, promise
+    ]);
+};

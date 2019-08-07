ca6e3a36d6c187fa7e028a9f97f51bd366a99555
franias <franciellipdias@gmail.com>
2019-08-07 16:40:09 -0300

    Usa o pipe parcializando as funcoes usadas para refatorar a funcao action

--- public/app/app.js
+++ public/app/app.js
@@ -1,22 +1,18 @@
 import { log } from './utils/promise_helpers.js';
 import './utils/array_helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime, partialize } from './utils/operators.js'
+import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js'
 
-const partializedTakeUntil = partialize(takeUntil, 2);
-const doTake = partializedTakeUntil(() => console.log("oi"));
-doTake();
-doTake();
-doTake();
-doTake();
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)  
+);
 
-const action = debounceTime(500,
-  takeUntil(3, () =>
-    service
-      .sumItems('2143')
-      .then(console.log)
-      .catch(console.log)
-  )
+const action = operations(() =>
+  service
+    .sumItems('2143')
+    .then(console.log)
+    .catch(console.log)
 );
 
 document

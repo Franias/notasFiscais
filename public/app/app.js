6def111cd3453259dd70ca57ee200a21d12fefe7
franias <franciellipdias@gmail.com>
2019-08-12 09:53:48 -0300

    Aplica desaclopamento de codigo com EventEmitter que sera chamado para fazer o padrao publish/subscribe

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,7 @@ import { log, timeOutPromise, retry } from './utils/promise_helpers.js';
 import './utils/array_helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js'
+import { EventEmitter } from './utils/event-emitter.js'
 
 const operations = pipe(
   partialize(takeUntil, 3),
@@ -10,10 +11,14 @@ const operations = pipe(
 
 const action = operations(() =>
   retry(3, 3000, () => timeOutPromise(200, service.sumItems('2143')))
-    .then(console.log)
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 );
 
-document 
+document
   .querySelector("#myButton")
   .onclick = action;
+
+  EventEmitter.on('itensTotalizados', console.log);
+  EventEmitter.on('itensTotalizados', total=> alert(total));
+
